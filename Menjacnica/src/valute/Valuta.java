d7078c8e93a34c35e53d58a26216d7a84f2588aa
LukaBaljak1995 <luk.abaljak@hotmail.com>
2016-03-19 13:34:23 +0100

    Dodati privatni atributi za kupovni, prodajni i srednji kurs.

--- Menjacnica/src/valute/Valuta.java
+++ Menjacnica/src/valute/Valuta.java
@@ -8,6 +8,33 @@ public class Valuta {
 	private String skraceniNaziv;
 
 	private GregorianCalendar datum;
+	private double prodajniKurs;
+	private double kupovniKurs;
+	private double srednjiKurs;
+	
+	public double getProdajniKurs() {
+		return prodajniKurs;
+	}
+
+	public double getKupovniKurs() {
+		return kupovniKurs;
+	}
+
+	public double getSrednjiKurs() {
+		return srednjiKurs;
+	}
+
+	public void setProdajniKurs(double prodajniKurs) {
+		this.prodajniKurs = prodajniKurs;
+	}
+
+	public void setKupovniKurs(double kupovniKurs) {
+		this.kupovniKurs = kupovniKurs;
+	}
+
+	public void setSrednjiKurs(double srednjiKurs) {
+		this.srednjiKurs = srednjiKurs;
+	}
 
 	public String getNaziv() {
 		return naziv;
