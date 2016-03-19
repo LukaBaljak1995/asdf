a64182e03b5bd235545af9001c711305762a9353
LukaBaljak1995 <luk.abaljak@hotmail.com>
2016-03-19 14:04:12 +0100

    Dodat je paket implementaciju u okviru koje je kreirana klasa Implementacija koja implementira metode Interfejsa ali bez tacnog nacina.

---
+++ Menjacnica/src/implementacija/Implementacija.java
@@ -0,0 +1,24 @@
+package implementacija;
+
+import java.util.GregorianCalendar;
+
+import interfejsi.Interfejs;
+
+public class Implementacija implements Interfejs {
+
+	@Override
+	public void dodajKurs(double kurs, GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public void obrisiKurs(double kurs, GregorianCalendar datum) {
+
+	}
+
+	@Override
+	public double pronadjiKurs(String naziv, GregorianCalendar datum) {
+		return 0;
+	}
+
+}
